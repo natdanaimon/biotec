f967923df387914c0f63dcaa79f779f8b0ef7bc2
natdanaimon <natdanaimon@gmail.com>
2017-02-05 04:37:39 +0700

    modify function checkdupplicate

--- service/newsletterService.php
+++ service/newsletterService.php
@@ -21,7 +21,7 @@ class newsletterService {
     function check_dupplicate($email) {
         require_once('../manage/bio/common/ConnectDB.php');
         $db = new ConnectDB();
-        $sql = " SELECT * tb_newsletter "
+        $sql = " SELECT * FROM tb_newsletter "
                 . " WHERE s_email = '" . $email . "' ";
         $_data = $db->Search_Data_FormatJson($sql);
         $db->close_conn();
