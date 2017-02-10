82db155960b08375f9ca3f86fffeae68ea1405dc
natdanaimon <natdanaimon@gmail.com>
2017-02-11 05:10:40 +0700

    slide status

--- service/slideService.php
+++ service/slideService.php
@@ -5,7 +5,7 @@ class slideService {
     function dataTable($_page) {
         require_once('./manage/bio/common/ConnectDB.php');
         $db = new ConnectDB();
-        $sql = " SELECT * FROM  tb_slide where s_page='$_page' order by i_index asc , s_img asc ";
+        $sql = " SELECT * FROM  tb_slide where s_page='$_page' and s_status ='A' order by i_index asc , s_img asc ";
         $_data = $db->Search_Data_FormatJson($sql);
         $db->close_conn();
         return $_data;
