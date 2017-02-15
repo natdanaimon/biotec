69b95cb87b52ebbc6d9b88e487ed35ef5af5fe63
natdanaimon <natdanaimon@gmail.com>
2017-02-15 16:15:25 +0700

    social
    social

---
+++ manage/bio/common/social.php
@@ -0,0 +1,68 @@
+<?php
+
+class social {
+
+    function __construct() {
+        $this->initial_social();
+    }
+
+    public function initial_social() {
+        $var = "<!-- initial facebook SDK -->";
+        $var .= "<div id=\"fb-root\"></div> ";
+        $var .= "<script>(function(d, s, id) { ";
+        $var .= "  var js, fjs = d.getElementsByTagName(s)[0]; ";
+        $var .= "  if (d.getElementById(id)) return; ";
+        $var .= "  js = d.createElement(s); js.id = id; ";
+        $var .= "  js.src = \"//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1308539565850437\"; ";
+        $var .= "  fjs.parentNode.insertBefore(js, fjs); ";
+        $var .= " }(document, 'script', 'facebook-jssdk'));</script>";
+        $var .= "<!-- initial facebook SDK --> </br>";
+
+        $var .= "<!-- initial script google+ -->";
+        $var .= "<script src = \"https://apis.google.com/js/platform.js\" async defer></script>";
+        $var .= "<!-- initial script google+ --></br>";
+
+        $var .= "<!-- initial script twitter -->";
+        $var .= "<script src=\"http://platform.twitter.com/widgets.js\" type=\"text/javascript\"></script>";
+        $var .= "<!-- initial script twitter --></br>";
+        echo $var;
+    }
+
+    public function facebook_like_button($url, $_share) {
+        $share = (!is_bool($_share) ? "true" : strtolower($_share));
+        $var = "<!-- facebook like button -->";
+        $var .= "<div class=\"fb-like\" ";
+        $var .= "	data-href=\"" . $url . "\"  ";
+        $var .= "	data-header=\"true\"  ";
+        $var .= "	data-layout=\"button_count\"  ";
+        $var .= "	data-action=\"like\"  ";
+        $var .= "	data-size=\"small\"  ";
+        $var .= "	data-show-faces=\"true\"  ";
+        $var .= "	data-share=\"" . $share . "\"> ";
+        $var .= "</div> ";
+        $var .= "<!-- facebook like button --> ";
+        echo $var;
+    }
+
+    public function googlePlus_Share_button($url) {
+        $var = "<!-- script google+ -->";
+        $var .="  <div class=\"g-plusone\" data-size=\"medium\" data-annotation=\"none\" data-href=\"" . $url . "\"></div>";
+        $var .="<!-- script google+ --> ";
+        echo $var;
+    }
+
+    public function twitter_Share_button($msg) {
+        $var = "<!-- script twitter -->";
+        $var .="    <a class=\"twitter-share-button\" ";
+        $var .="   data-size = \"small\" ";
+        $var .="   data-text = \"" . $msg . "\" ";
+//        $var .="   data-hashtags = \"biotecitalia-thailand\" ";
+//        $var .="   data-via = \"biotecitalia-thailand.com\" ";
+        $var .="   data-related = \"twitterapi,twitter\"> ";
+        $var .="   Tweet ";
+        $var .="   </a > ";
+        $var .="<!-- script twitter --> ";
+        echo $var;
+    }
+
+}
