4c0f39408023b68f3df0d5c0543ce42186a2bff7
natdanaimon <natdanaimon@gmail.com>
2017-02-15 16:16:31 +0700

    social
    social

---
+++ testSocial.php
@@ -0,0 +1,32 @@
+
+<html>
+    <head>
+        <title>Your Website Title</title>
+
+    </head>
+    <body>
+
+        <?php
+        
+        
+        echo "GET url : " . $_GET["url"];
+        if ($_GET["url"] != NULL) {
+            $link = $_GET["url"];
+        } else {
+            $link = "http://biotecitalia-thailand.com/newEmptyPHP.php";
+        }
+        include './manage/bio/common/social.php';
+        $social = new social();
+        echo $social->twitter_Share_button("test message share twitter biotecitalia-thailand.com");
+        echo $social->googlePlus_Share_button($link);
+        echo $social->facebook_like_button($link, FALSE);
+        
+        
+        
+        ?>
+
+
+
+    </body>
+</body>
+</html>
