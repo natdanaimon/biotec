27b24347c2166841d8faaea471e1979b9460af84
operty <xuwunmaitee@gmail.com>
2017-02-20 23:39:28 +0700

    edit news' service
    edit news' service

--- service/newsService.php
+++ service/newsService.php
@@ -5,8 +5,8 @@ class newsService {
     function dataTable() {
         require_once('./manage/bio/common/ConnectDB.php');
         $db = new ConnectDB();
-        $sql = " SELECT tb_news.*,tb_news_img.*  FROM  tb_news "
-                . " LEFT JOIN tb_news_img ON "
+        $sql = " SELECT tb_news.*,tb_news_img.*  FROM  tb_news  "
+                . " INNER JOIN tb_news_img ON "
                 . " tb_news.s_seq = tb_news_img.s_seq WHERE tb_news.s_status = 'A' ";
         $_data = $db->Search_Data_FormatJson($sql);
         $db->close_conn();
@@ -16,9 +16,12 @@ class newsService {
     function dataTable_type($type_i) {
         require_once('./manage/bio/common/ConnectDB.php');
         $db = new ConnectDB();
-        $sql = " SELECT * FROM  tb_news n "
+        /*$sql = " SELECT * FROM  tb_news n "
                 . " WHERE n.s_status = 'A' "
-                . " AND s_category = '" . $type_i . "' ";
+                . " AND s_category = '" . $type_i . "' ";*/
+        $sql = " SELECT tb_news.*,tb_news_img.*  FROM  tb_news  "
+                . " INNER JOIN tb_news_img ON "
+                . " tb_news.s_seq = tb_news_img.s_seq WHERE tb_news.s_status = 'A' AND s_category = '" . $type_i . "' ";
         $_data = $db->Search_Data_FormatJson($sql);
         $db->close_conn();
         return $_data;
@@ -26,22 +29,21 @@ class newsService {
 
     function dataTable_pic($seq) {
 
-            require_once('./manage/bio/common/ConnectDB.php');
-            $db = new ConnectDB();
-            $sql = " SELECT * FROM `tb_news_img` WHERE s_seq='".$seq."'";
-            $_data = $db->Search_Data_FormatJson($sql);
-            $db->close_conn();
-            return $_data;
-        
-
+        require_once('./manage/bio/common/ConnectDB.php');
+        $db = new ConnectDB();
+        $sql = " SELECT * FROM `tb_news_img` WHERE s_seq='" . $seq . "'";
+        $_data = $db->Search_Data_FormatJson($sql);
+        $db->close_conn();
+        return $_data;
     }
 
     function query_dataTable($id) {
         require_once('./manage/bio/common/ConnectDB.php');
         $db = new ConnectDB();
-        $sql = " SELECT * FROM  tb_news n "
-                . " WHERE n.s_status = 'A' "
-                . " AND n.s_seq = '" . $id . "'";
+        $sql = " SELECT tb_news.*,tb_news_img.*  FROM  tb_news  "
+                . " INNER JOIN tb_news_img ON "
+                . " tb_news.s_seq = tb_news_img.s_seq "
+                . " WHERE tb_news.s_status = 'A' AND tb_news.s_seq = '" . $id . "'";
         $_data = $db->Search_Data_FormatJson($sql);
         $db->close_conn();
         return $_data;
